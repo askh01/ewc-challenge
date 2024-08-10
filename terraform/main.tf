0b520885792bb2642abed66f5457d80952eaaab7
Asadullah Khalid <kasadullahkhalid@gmail.com>
2024-08-10 02:11:37 +0200

    feat: Add terraform IaC

---
+++ terraform/main.tf
@@ -0,0 +1,95 @@
+# Specify the AWS provider
+provider "aws" {
+  region = "eu-central-1" # Change to your preferred region
+}
+
+# Create a VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+# Create a Subnet
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-central-1a" # Change to your preferred availability zone
+
+  tags = {
+    Name = "main-subnet"
+  }
+}
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+# Create a Route Table
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "main-route-table"
+  }
+}
+
+# Associate the Route Table with the Subnet
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
+
+# Create a Security Group that allows SSH and HTTP access
+resource "aws_security_group" "main" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "main-sg"
+  }
+}
+
+# Create the EC2 instance
+resource "aws_instance" "web" {
+  ami             = "ami-00060fac2f8c42d30" # Amazon Linux 2 AMI for eu-central-1 region
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.main.id]
+
+  tags = {
+    Name = "WebServerInstance"
+  }
+}
