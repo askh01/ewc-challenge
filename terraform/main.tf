c9cd62899729c17b296852a257ddb05a156f4c56
Asadullah Khalid <kasadullahkhalid@gmail.com>
2024-08-11 15:42:01 +0200

    feat: Add private subnet and security groups for backend DB services

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,6 +25,16 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "eu-central-1a" # Change to your preferred availability zone
+  map_public_ip_on_launch = true            # automatically assign publlic ip to any ec2 launched in the public subnet
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
 # Create an Internet Gateway
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
@@ -35,7 +45,7 @@ resource "aws_internet_gateway" "main" {
 }
 
 # Create a Route Table
-resource "aws_route_table" "main" {
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   route {
@@ -44,19 +54,21 @@ resource "aws_route_table" "main" {
   }
 
   tags = {
-    Name = "main-route-table"
+    Name = "public-rt"
   }
 }
 
-# Associate the Route Table with the Subnet
-resource "aws_route_table_association" "main" {
+# Associate the Route Table with the public Subnet
+resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.main.id
+  route_table_id = aws_route_table.public.id
 }
 
 # Create a Security Group that allows SSH and HTTP access
-resource "aws_security_group" "frontend-ec2" {
+resource "aws_security_group" "frontend_sg" {
   vpc_id = aws_vpc.main.id
+
+# Allow SSH access (if needed)
 
   ingress {
     from_port   = 22
@@ -64,21 +76,14 @@ resource "aws_security_group" "frontend-ec2" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+# Allow HTTP access to the 'vote' service
   ingress {
     from_port   = 5000
     to_port     = 5000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+# Allow HTTP access to the 'result' service
   ingress {
     from_port   = 5001
     to_port     = 5001
@@ -94,9 +99,81 @@ resource "aws_security_group" "frontend-ec2" {
   }
 
   tags = {
-    Name = "frontend-ec2-sg"
+    Name = "frontend-sg"
   }
 }
+
+resource "aws_security_group" "backend_sg" {
+  vpc_id = aws_vpc.main.id
+
+# Allow SSH access (if needed)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow frontend services to access Redis (if needed directly by the application)
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    security_groups = [aws_security_group.frontend_sg.id]  # Allowing access from frontend to Redis
+  }
+
+  # Allow communication within the backend security group
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    self = true
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "backend-sg"
+  }
+}
+
+resource "aws_security_group" "db_sg" {
+  vpc_id = aws_vpc.main.id
+
+# Allow SSH access (if needed)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow access from the backend security group (e.g., worker, redis) to the DB
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    security_groups = [aws_security_group.backend_sg.id]  # DB access from backend services only
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "db-sg"
+  }
+}
+
+
 
 resource "aws_key_pair" "aws_frontend_ec2_keypair" {
   key_name   = "aws_frontend_ec2_keypair"
@@ -108,7 +185,7 @@ resource "aws_instance" "web" {
   ami                    = "ami-07652eda1fbad7432" # Ubuntu AMI for eu-central-1 region
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_subnet.id
-  vpc_security_group_ids = [aws_security_group.frontend-ec2.id]
+  vpc_security_group_ids = [aws_security_group.frontend_sg.id]
   key_name               = "aws_frontend_ec2_keypair"
 
   user_data = <<-EOF
